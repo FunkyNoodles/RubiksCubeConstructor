b5e0688172653ce124e097fee25f6ab49a737525
Funky Noodles <nickasds@hotmail.com>
2017-05-18 15:27:53 -0500

    tried using both saturation and volumn channels for edge detection

--- RubiksCubeConstructor/main.cpp
+++ RubiksCubeConstructor/main.cpp
@@ -38,6 +38,24 @@ static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
 	return (dx1*dx2 + dy1*dy2) / sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
 
+void colorReduce(cv::Mat& image, int div = 64)
+{
+	int nl = image.rows;                    // number of lines
+	int nc = image.cols * image.channels(); // number of elements per line
+
+	for (int j = 0; j < nl; j++)
+	{
+		// get the address of row j
+		uchar* data = image.ptr<uchar>(j);
+
+		for (int i = 0; i < nc; i++)
+		{
+			// process each pixel
+			data[i] = data[i] / div * div + div / 2;
+		}
+	}
+}
+
 // returns sequence of squares detected on the image.
 // the sequence is stored in the specified memory storage
 static void findSquares(const cv::Mat & image, std::vector<std::vector<cv::Point> >& squares)
@@ -47,78 +65,82 @@ static void findSquares(const cv::Mat & image, std::vector<std::vector<cv::Point
 	cv::Mat pyr, timg, gray0(image.size(), CV_8U), gray;
 
 	// down-scale and upscale the image to filter out the noise
-	pyrDown(image, pyr, cv::Size(image.cols / 2, image.rows / 2));
-	pyrUp(pyr, timg, image.size());
+	//pyrDown(image, pyr, cv::Size(image.cols / 2, image.rows / 2));
+	//pyrUp(pyr, timg, image.size());
 	std::vector<std::vector<cv::Point> > contours;
 	std::vector<cv::Vec4i> hierarchy;
 
 	// find squares in every color plane of the image
 	//mixChannels(&timg, 1, &gray0, 1, ch, 1);
-	//GaussianBlur(timg, timg, Size(7, 7), 20, 20, BORDER_DEFAULT);
+	GaussianBlur(image, timg, cv::Size(11, 11), 3, 3, cv::BORDER_DEFAULT);
 	
-	cvtColor(timg, gray0, CV_BGR2GRAY);
-
-	// Apply theshold
-	//cv::threshold(gray0, gray0, thresh, 255, CV_THRESH_TRUNC);
-	cv::imshow(WIN_TEST1, gray0);
-
 	cv::Mat imageHSV;
 	cvtColor(timg, imageHSV, CV_BGR2HSV);
+	cv::Mat hsvChannels[3];
+	cv::split(imageHSV, hsvChannels);
 
-	// Denoise using cuda
-	cv::cuda::GpuMat gray0Device, gray0DeviceDst;
-	gray0Device.upload(gray0);
-	cv::cuda::fastNlMeansDenoising(gray0Device, gray0DeviceDst, 10);
-	gray0DeviceDst.download(gray0);
-	//fastNlMeansDenoising(gray0, gray0);
-	
-	// try several threshold levels
-	//for (int l = 0; l < N; l++)
-	//{
-	// hack: use Canny instead of zero threshold level.
-	// Canny helps to catch squares with gradient shading
-	Canny(gray0, gray, 0, thresh, 5);
-	//dilate(gray, gray, Mat(), Point(-1, -1));
-	imshow(WIN_VIDEO, gray);
-	
-
-	// find contours and store them all as a list
-	findContours(gray, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-
-	std::vector<cv::Point> approx;
-
-	// test each contour
-	//std::cout << contours.size() << std::endl;
-	for (size_t i = 0; i < contours.size(); i++)
-	{
-		// approximate contour with accuracy proportional
-		// to the contour perimeter
-		approxPolyDP(cv::Mat(contours[i]), approx, arcLength(cv::Mat(contours[i]), true)*0.02, true);
+	for (int c = 1; c < 3; ++c){
 		
-		// square contours should have 4 vertices after approximation
-		// relatively large area (to filter out noisy contours)
-		// and be convex.
-		// Note: absolute value of an area is used because
-		// area may be positive or negative - in accordance with the
-		// contour orientation
-		if (approx.size() == 4 &&
-			fabs(cv::contourArea(cv::Mat(approx))) > 100
-			&& cv::isContourConvex(cv::Mat(approx)))
+		hsvChannels[c].copyTo(gray0);
+		cv::imshow(WIN_TEST1, gray0);
+	
+		// Denoise using cuda
+		//hsvChannels[1].copyTo(gray0);
+		cv::cuda::GpuMat gray0Device, gray0DeviceDst;
+		gray0Device.upload(gray0);
+		cv::cuda::fastNlMeansDenoising(gray0Device, gray0DeviceDst, 10);
+		gray0DeviceDst.download(gray0);
+
+	
+	
+		// try several threshold levels
+		//for (int l = 0; l < N; l++)
+		//{
+		// hack: use Canny instead of zero threshold level.
+		// Canny helps to catch squares with gradient shading
+		Canny(gray0, gray, 20, thresh, 5);
+		//dilate(gray, gray, Mat(), Point(-1, -1));
+		//imshow(WIN_VIDEO, gray);
+	
+
+		// find contours and store them all as a list
+		findContours(gray, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+		std::vector<cv::Point> approx;
+
+		// test each contour
+		//std::cout << contours.size() << std::endl;
+		for (size_t i = 0; i < contours.size(); i++)
 		{
-			double maxCosine = 0;
-
-			for (int j = 2; j < 5; j++)
+			// approximate contour with accuracy proportional
+			// to the contour perimeter
+			approxPolyDP(cv::Mat(contours[i]), approx, arcLength(cv::Mat(contours[i]), true)*0.02, true);
+		
+			// square contours should have 4 vertices after approximation
+			// relatively large area (to filter out noisy contours)
+			// and be convex.
+			// Note: absolute value of an area is used because
+			// area may be positive or negative - in accordance with the
+			// contour orientation
+			if (approx.size() == 4 &&
+				fabs(cv::contourArea(cv::Mat(approx))) > 100
+				&& cv::isContourConvex(cv::Mat(approx)))
 			{
-				// find the maximum cosine of the angle between joint edges
-				double cosine = fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
-				maxCosine = MAX(maxCosine, cosine);
-			}
+				double maxCosine = 0;
 
-			// if cosines of all angles are small
-			// (all angles are ~90 degree) then write quandrange
-			// vertices to resultant sequence
-			//if (maxCosine < 0.5)
-				squares.push_back(approx);
+				for (int j = 2; j < 5; j++)
+				{
+					// find the maximum cosine of the angle between joint edges
+					double cosine = fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
+					maxCosine = MAX(maxCosine, cosine);
+				}
+
+				// if cosines of all angles are small
+				// (all angles are ~90 degree) then write quandrange
+				// vertices to resultant sequence
+				//if (maxCosine < 0.5)
+					squares.push_back(approx);
+			}
 		}
 	}
 }
@@ -139,19 +161,19 @@ static void drawSquares(cv::Mat& image, const std::vector<std::vector<cv::Point>
 
 int main(int arg, char ** argv) {
 
-	Cube * goalCube = new Cube(3);
-	Cube * cube = new Cube(3);
-	cube->shuffle(3);
-	//std::cout << "Test: " << (*cube == *goalCube) << std::endl;
-	//std::cout << sizeof(size_t) << " bytes" << std::endl;
-	//std::cout << cube->getHeuristic(HeuristicType::MISPLACED, *goalCube) << std::endl;
-	cube->aStar(*goalCube);
-	std::cout << "Done" << std::endl;
-	return 0 ;
-	while (true)
-	{
+	//Cube * goalCube = new Cube(3);
+	//Cube * cube = new Cube(3);
+	//cube->shuffle(3);
+	////std::cout << "Test: " << (*cube == *goalCube) << std::endl;
+	////std::cout << sizeof(size_t) << " bytes" << std::endl;
+	////std::cout << cube->getHeuristic(HeuristicType::MISPLACED, *goalCube) << std::endl;
+	//cube->aStar(*goalCube);
+	//std::cout << "Done" << std::endl;
+	//return 0 ;
+	//while (true)
+	//{
 
-	}
+	//}
 	char keyIn;
 
 	cv::VideoCapture cam(0);
@@ -182,26 +204,6 @@ int main(int arg, char ** argv) {
 			std::cout << "No input";
 			break;
 		}
-
-		// Reduce noise amd convert to gray
-		GaussianBlur(curFrame, curFrameBlurred, cv::Size(5, 5), 0, 0, cv::BORDER_DEFAULT);
-		cvtColor(curFrameBlurred, curFrameGray, CV_BGR2GRAY);
-
-		// Generate gradX and gradY
-		cv::Mat gradX, gradY;
-		cv::Mat absGradX, absGradY;
-
-		// Gradient X
-		//Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-		cv::Sobel(curFrameGray, gradX, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT);
-		cv::convertScaleAbs(gradX, absGradX);
-
-		// Gradient Y
-		//Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-		Sobel(curFrameGray, gradY, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT);
-		convertScaleAbs(gradY, absGradY);
-
-		addWeighted(absGradX, 0.5, absGradY, 0.5, 0, curGrad);
 
 		//dilate(curGrad, curGrad, kernelDilation);
 
